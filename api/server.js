e802ca23237dfd705d9e3a50dbe474fbf68f2ac8
ciiiii <allen.ccccnm@gmail.com>
2022-04-02 20:01:30 +0800

    config server.js as serverless functions

--- api/server.js
+++ api/server.js
@@ -2,7 +2,14 @@ var express = require("express");
 var app = express();
 const jsonServer = require("json-server");
 
-const router = jsonServer.router("./db.json");
+const router = jsonServer.router(
+  process.env.DB
+    ? process.env.DB
+    : {
+        selected: [],
+        comments: [],
+      }
+);
 
 app.use(express.static("public"));
 app.use(router);
@@ -11,7 +18,8 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.listen(process.env.PORT, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log(`App listening on port ${process.env.PORT}`);
 });
 
